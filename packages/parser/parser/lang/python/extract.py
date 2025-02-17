75c151a20f71b4b3b1dd65c6e78c9d588fb36bc4
Mark Shteyn <markshteyn1@gmail.com>
2025-02-16 23:41:29 -0500

    extracted name, params, & body

--- packages/parser/parser/lang/python/extract.py
+++ packages/parser/parser/lang/python/extract.py
@@ -2,12 +2,15 @@ import tree_sitter_python as tspython
 from tree_sitter import Language, Parser
 from parser.exceptions import InvalidFunctionException
 from .validate import validate_fn
+from typing import List, Tuple
 
 PY_LANGUAGE = Language(tspython.language())
 parser = Parser(PY_LANGUAGE)
 
+type ExtractionResult = Tuple[str, List[str], str]
 
-def extract_fn(code: str):
+
+def extract_fn(code: str) -> ExtractionResult:
     code_bytes = bytes(code, "utf8")
     tree = parser.parse(code_bytes, encoding="utf8")
 
@@ -15,6 +18,7 @@ def extract_fn(code: str):
         raise InvalidFunctionException()
 
     fn = tree.root_node.children[0]
-    print(fn)
-    name = fn.child_by_field_name("name")
-    print(f"Extracted function: {name}")
+    name = fn.child_by_field_name("name").text.decode("utf8")
+    params = fn.child_by_field_name("parameters").text.decode("utf8")[1:-1].split(", ")
+    body = fn.child_by_field_name("body").text.decode("utf8")
+    return name, params, body
--- packages/parser/tests/test_parse.py
+++ packages/parser/tests/test_parse.py
@@ -3,8 +3,9 @@ from parser.lang.python import extract_python_fn
 
 def test_extract_python_fn():
     fn = """
-    def print_message(message):
-        print(message)
+    def print_message(message, times):
+        for _ in range(times):
+            print(message)
     """
-    print(extract_python_fn(fn))
+    extract_python_fn(fn)
     
