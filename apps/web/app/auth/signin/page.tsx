89c9e8fa0701951931b218ccec868491da20dd19
Mark Shteyn <markshteyn1@gmail.com>
2025-05-26 10:54:40 -0400

    added gitlab oauth

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -1,14 +1,15 @@
-import { signIn } from "@/lib/auth";
 
+import { signIn } from "@/lib/auth"
+ 
 export default function SignIn() {
   return (
     <form
       action={async () => {
-        "use server";
-        await signIn("bitbucket");
+        "use server"
+        await signIn("bitbucket")
       }}
     >
       <button type="submit">Signin with Bitbucket</button>
     </form>
-  );
-}
+  )
+} 
--- apps/web/config/auth.config.ts
+++ apps/web/config/auth.config.ts
@@ -1,8 +1,8 @@
 import GitHub from "next-auth/providers/github"
 import Bitbucket from "next-auth/providers/bitbucket"
+import GitLab from "next-auth/providers/gitlab"
 import type { NextAuthConfig } from "next-auth"
 
-// Notice this is only an object, not a full Auth.js instance
 export default {
-    providers: [GitHub, Bitbucket],
+    providers: [GitHub, Bitbucket, GitLab],
 } satisfies NextAuthConfig
